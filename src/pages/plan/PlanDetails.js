6f82f04ecc5026eaa73b338d44a41983716e4efe
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-15 19:30:19 +0000

    FEAT: Message displayed to users who have no focus areas yet

--- src/pages/plan/PlanDetails.js
+++ src/pages/plan/PlanDetails.js
@@ -9,6 +9,7 @@ import Card from 'react-bootstrap/Card';
 import Spinner from 'react-bootstrap/Spinner';
 import { useAccordionToggle } from 'react-bootstrap';
 import miscellaneousImage from '../../assets/miscellaneous-tasks.jpg';
+import defaultImage from '../../assets/default-focus.jpg';
 import { axiosReq } from '../../api/axiosDefaults';
 import FocusHighlightMobile from '../focus/FocusHighlightMobile';
 import FocusHighlightDesktop from '../focus/FocusHighlightDesktop';
@@ -71,21 +72,45 @@ const PlanDetails = ({mobile}) => {
         <>
           {mobile ? (
             <Accordion className={accStyles.Accordion}>
-              {focuses.results.length && (
+              {focuses.results.length>0 ? (
                 focuses.results.map(focus => (
                   <FocusHighlightMobile key={focus.id} {...focus} />
                 ))
+              ) : (
+                <Card>
+                  <Card.Header>
+                    <ContextAwareToggle as={Card.Header} eventKey="0">
+                      <div className={accStyles.Title}>
+                        <img className={accStyles.Image} src={defaultImage} alt='focus'/>
+                        <h2>Your focus areas</h2>
+                      </div>
+                    </ContextAwareToggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      <p>Define the areas of your life you wish to focus on by creating focus areas.</p>
+                      <p>Once created all your focus areas will be viewable here in your plan.</p>
+                      <p>Within each focus area, you can set yourself goals and add the tasks you need to complete to achieve them, as well as any day to day tasks linked with that area of your life.</p>
+                      <div className={cardStyles.ButtonContainer}>
+                        <Link className={`${btnStyles.Button} ${cardStyles.Button}`} to={'/focus/create'}>
+                          Create your first focus area
+                        </Link>
+                      </div>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
               )}
+
               <Card>
                 <Card.Header>
-                  <ContextAwareToggle as={Card.Header} eventKey="0">
+                  <ContextAwareToggle as={Card.Header} eventKey="99">
                     <div className={accStyles.Title}>
-                      <img className={accStyles.Image} src={miscellaneousImage} alt='focus'/>
+                      <img className={accStyles.Image} src={miscellaneousImage} alt='miscellaneous'/>
                       <h2>Miscellaneous</h2>
                     </div>
                   </ContextAwareToggle>
                 </Card.Header>
-                <Accordion.Collapse eventKey="0">
+                <Accordion.Collapse eventKey="99">
                   <Card.Body>
                     <p>A place for tasks that don't fit into any of your focus areas.</p>
                     <div className={cardStyles.ButtonContainer}>
@@ -99,10 +124,29 @@ const PlanDetails = ({mobile}) => {
             </Accordion>
           ) : (
             <div className={cardStyles.CardsContainer}>
-              {focuses.results.length && (
+              {focuses.results.length>0 ? (
                 focuses.results.map(focus => (
                   <FocusHighlightDesktop key={focus.id} {...focus} />
                 ))
+              ) : (
+                <div className={cardStyles.Card}>
+                  <div className={cardStyles.Header}>
+                    <img className={cardStyles.Image} src={defaultImage} alt='focus'/>
+                    <div className={cardStyles.Title}>
+                      <h2>Your focus areas</h2>
+                    </div>
+                  </div>
+                  <div className={cardStyles.GoalContainer}>
+                    <p>Define the areas of your life you wish to focus on by creating focus areas.</p>
+                    <p>Once created all your focus areas will be viewable here in your plan.</p>
+                    <p>Within each focus area, you can set yourself goals and add the tasks you need to complete to achieve them, as well as any day to day tasks linked with that area of your life.</p>
+                  </div>
+                  <div className={cardStyles.ButtonContainer}>
+                    <Link className={`${btnStyles.Button} ${cardStyles.Button}`} to={'/focus/create'}>
+                      Create your first focus area
+                    </Link>
+                  </div>
+                </div>
               )}
               <div className={cardStyles.Card}>
                 <div className={cardStyles.Header}>
