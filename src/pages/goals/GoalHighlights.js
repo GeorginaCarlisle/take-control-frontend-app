5d0f7ffff62edf6b2418ae4eab265646b435ce10
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-22 16:25:38 +0000

    FEAT: Adjust how the goal field days_remaining is shown

--- src/pages/goals/GoalHighlights.js
+++ src/pages/goals/GoalHighlights.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import cardStyles from '../../styles/Cards.module.css';
+import styles from '../../styles/Goal.module.css';
 
 const GoalHighlights = (props) => {
 
   const {
     title,
     deadline,
+    deadline_near,
+    days_remaining,
     description,
   } = props;
 
@@ -15,7 +18,18 @@ const GoalHighlights = (props) => {
         <h3>{title}</h3>
         <span>{deadline}</span>
       </div>
-      <p>{description}</p>
+      {description && <p>{description}</p>}
+      {deadline_near && (
+          days_remaining===0 || days_remaining===1 ? (
+            <p className={styles.HighlightDeadlineWarning}>DEADLINE DUE</p>
+          ) : (
+            days_remaining<0 ? (
+              <p className={styles.HighlightDeadlineWarning}>DEADLINE OVERDUE !!</p>
+            ) : (
+              <p className={styles.HighlightDeadlineWarning}>Deadline near only {days_remaining} days remaining</p>
+            )
+          )
+        )}
     </div>
   )
 }
