64af52ff465f4b76b1b864232ef4d91e04671adf
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-22 14:50:36 +0000

    FIX: Incorrect class syntax in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,14 +16,14 @@ const Footer = () => {
           rel="noreferrer" 
           aria-label="Visit my GitHub profile (opens in a new tab)"
           className={styles.Icons}>
-          <i class="fa-brands fa-github"></i>
+          <i className="fa-brands fa-github"></i>
           </a>
           <a href="https://www.linkedin.com/in/georgina-carlisle-617b58268" 
           target="_blank" 
           rel="noreferrer" 
           aria-label="Visit my linkedin profile (opens in a new tab)"
           className={styles.Icons}>
-          <i class="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-linkedin"></i>
           </a>
       </span>
     </footer>
