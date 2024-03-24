ee1644f2b7bd19df90a27b6d2960f9a6994f9ee6
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-24 12:47:39 +0000

    FIX: Pass through checkedUser to all protected routes where needed

--- src/pages/focus/Focus.js
+++ src/pages/focus/Focus.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import pageStyles from '../../styles/Page.module.css';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { useCheckedUser, useCurrentUser } from '../../contexts/CurrentUserContext';
 import MobileFocus from './MobileFocus';
 import DesktopFocus from './DesktopFocus';
 import { useParams } from 'react-router-dom/cjs/react-router-dom';
@@ -9,19 +9,28 @@ import { useParams } from 'react-router-dom/cjs/react-router-dom';
 const Focus = () => {
 
   const currentUser = useCurrentUser();
+  const checkedUser = useCheckedUser();
   const { id } = useParams();
 
   return (
     <div className={pageStyles.PageContainer}>
-      <div className={pageStyles.Title}>
-        <h1>{currentUser.username}'s Plan</h1>
-      </div>      
-      <div className={pageStyles.MobileOnly}>
-        <MobileFocus id={id}/>
-      </div>
-      <div className={pageStyles.DesktopOnly}>
-        <DesktopFocus id={id}/>
-      </div>
+      {checkedUser ? (
+        <>
+          <div className={pageStyles.Title}>
+            <h1>{currentUser.username}'s Plan</h1>
+          </div>      
+          <div className={pageStyles.MobileOnly}>
+            <MobileFocus id={id}/>
+          </div>
+          <div className={pageStyles.DesktopOnly}>
+            <DesktopFocus id={id}/>
+          </div>
+        </>
+      ) : (
+        <div>
+          Just loading your data ....
+        </div>
+      )}
     </div>  
   )
 }
--- src/pages/plan/Miscellaneous.js
+++ src/pages/plan/Miscellaneous.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../../styles/Miscellaneous.module.css';
 import pageStyles from '../../styles/Page.module.css';
 import btnStyles from '../../styles/Button.module.css';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { useCheckedUser, useCurrentUser } from '../../contexts/CurrentUserContext';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 import miscellaneousImage from '../../assets/miscellaneous-tasks.jpg';
 import TaskList from '../tasks/TaskList';
@@ -11,6 +11,7 @@ import TaskList from '../tasks/TaskList';
 const Miscellaneous = () => {
 
   const currentUser = useCurrentUser();
+  const checkedUser = useCheckedUser();
 
   const history = useHistory();
 
@@ -20,21 +21,29 @@ const Miscellaneous = () => {
 
   return (
     <div className={pageStyles.PageContainer}>
-      <div className={pageStyles.Title}>
-        <h1>{currentUser.username}'s Plan</h1>
-      </div>      
-      <div className={`${pageStyles.ContentContainer} ${styles.MainContainer}`}>
-        <button className={btnStyles.BackCross} aria-label="Click to return to the plan" onClick={handleBack}>
-          <i className="fa-solid fa-x"></i>
-        </button>
-        <div className={styles.Title}>
-          <img className={styles.Image} src={miscellaneousImage} alt='miscellaneous'/>
-          <h2>Miscellaneous</h2>
+      {checkedUser ? (
+        <>
+          <div className={pageStyles.Title}>
+            <h1>{currentUser.username}'s Plan</h1>
+          </div>      
+          <div className={`${pageStyles.ContentContainer} ${styles.MainContainer}`}>
+            <button className={btnStyles.BackCross} aria-label="Click to return to the plan" onClick={handleBack}>
+              <i className="fa-solid fa-x"></i>
+            </button>
+            <div className={styles.Title}>
+              <img className={styles.Image} src={miscellaneousImage} alt='miscellaneous'/>
+              <h2>Miscellaneous</h2>
+            </div>
+            <div className={styles.TaskList}>
+              <TaskList type="miscellaneous"/>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          Just loading your data ....
         </div>
-        <div className={styles.TaskList}>
-          <TaskList type="miscellaneous"/>
-        </div>
-      </div>
+      )}
     </div>  
   )
 }
